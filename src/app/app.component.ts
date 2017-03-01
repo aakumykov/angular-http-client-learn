4617f8a30360ba0027eb42518ed6dccb1d8b8c86
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-01 11:21:36 +0700

    Оставил только AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component }         from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app.html'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+	//private requestUri = 'https://api.github.com/users/aakumykov/repos'
+	private requestUri = 'http://localhost/rus_gods.json'
+
+	ngOnInit(){
+		console.info('AppComponent.ngOnInit()');
+	}
+
+
 }
--- src/app/toh/hero.ts
+++
@@ -1,7 +0,0 @@
-// А что, если без него?
-export class Hero {
-	constructor(
-		public id: number,
-		public name: string
-	){}
-}
