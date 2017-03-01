d906d46baf52e7db08e62cae6262e7e1199106d2
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-01 12:45:43 +0700

    Убрал импорт OnInit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app.html'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
 	//private requestUri = 'https://api.github.com/users/aakumykov/repos'
-	private requestUri = 'http://localhost/rus_gods.json'
+	// private requestUri = 'http://localhost/rus_gods.json'
 
 	ngOnInit(){
 		console.info('AppComponent.ngOnInit()');
 	}
-
-
 }
