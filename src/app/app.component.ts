5746383fde5e32b31196fb2576eb350dfcee95d5
Андрей Кумыков <aakumykov@yandex.ru>
2017-03-01 13:12:24 +0700

    Начало работать с access-control-allow-origin:"*"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
@@ -7,9 +9,28 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 	//private requestUri = 'https://api.github.com/users/aakumykov/repos'
-	// private requestUri = 'http://localhost/rus_gods.json'
+	private requestUri = 'http://localhost/rus_gods.json'
+	private res: any;
+
+	constructor(private http: Http){}
 
 	ngOnInit(){
 		console.info('AppComponent.ngOnInit()');
+
+		this.http.get(this.requestUri)
+			//.map(res => res.json())
+			.subscribe(
+				res => {
+					console.info('----- УСПЕХ -----');
+					console.info(res);
+					console.info('------------------');
+				},
+				error => {
+					console.info('----- ОШИБКА -----');
+					console.info(error.toString());
+					console.info('------------------');
+				},
+				() => console.info('~~~~~~~ запрос выполнен ~~~~~~~')
+			);
 	}
 }
